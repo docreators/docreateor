12e346fac2339fd7b8efa57271a74f9e0cd10b85
Tomasz Bawor <bawortomasz@gmail.com>
2020-03-29 18:29:00 +0200

    Add Controller

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,3 @@
-FROM gradle:6.3.0-jdk8 as builder
-USER root
-WORKDIR /builder
-ADD . /builder
-RUN gradle build --stacktrace
-
 FROM openjdk:8-jre-alpine
 WORKDIR /app
 EXPOSE 8080
--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,8 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+ 	// SPRING
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
